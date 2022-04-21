c852867284d5700134a310f8ea8ea3f92036bcbd
skotwal09 <skotwal09@gmail.com>
2022-04-21 18:19:25 -0400

    Update dataloader.py

--- code/dataset_processing/dataloader.py
+++ code/dataset_processing/dataloader.py
@@ -1,3 +1,4 @@
+from mpl_toolkits.mplot3d import Axes3D 
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,7 +13,7 @@ class Dataloader:
     
     def generate_coast_segments(self):
         plt.ioff()
-        m = Basemap(projection='robin',lon_0=0,resolution='c')
+        m = Basemap(projection='robin',lon_0=0,resolution='c',fix_aspect=False)
         coordinates = np.vstack(m.drawcoastlines().get_segments())
         x, y = m(coordinates[:,0],coordinates[:,1],inverse=True)
         plt.close()
